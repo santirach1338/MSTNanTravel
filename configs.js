9de5789580352f8bdf203cbc5265e3da2d780479
DESKTOP-JM9JF6D\FrienDz <kirito12589@gmail.com>
2022-05-16 23:31:21 +0700

    hotfix v1.1

--- configs.js
+++ configs.js
@@ -3,7 +3,7 @@
 const configs ={
     API_KEY:process.env.API_KEY,
     stpKEY:process.env.stpKEY,
-    listenPort:process.env.PORT || 3080
+    listenPort:process.env.PORT || 5000
 }
 
 module.exports = configs
