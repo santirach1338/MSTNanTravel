829d29925cb85e762bda5aafb6acdb785a2a06ad
DESKTOP-JM9JF6D\FrienDz <kirito12589@gmail.com>
2022-06-28 14:02:46 +0700

    upload

--- configs.js
+++ configs.js
@@ -1,15 +1,9 @@
 
 
-// const configs ={
-//     API_KEY:process.env.API_KEY,
-//     stpKEY:process.env.stpKEY,
-//     listenPort:process.env.PORT || 5000
-// }
-
 const configs ={
-    API_KEY:"AIzaSyDt78c-QEji3m2PtBU8CEJ60euER-j4Nxk",
-    stpKEY:"comSci63-RMUTL-NAN",
-    listenPort:3080
+    API_KEY:process.env.API_KEY,
+    stpKEY:process.env.stpKEY,
+    listenPort:process.env.PORT || 5000
 }
 
 module.exports = configs
