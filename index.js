1040dee27ad07a15819db9fd39d469c8c6822402
DESKTOP-OV888RF\santi <santirach1995@gmail.com>
2022-09-25 19:26:20 +0700

    update 1

--- index.js
+++ index.js
@@ -18,24 +18,6 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: "Hello API , By Santirach" })
 })
 
-// app.get("/api/test", (req, res) => {
-//     // ตัวอย่างข้อมูลสถานที่ //
-//     let listNode = [
-//         { nName: "Wat Phu min", geo: "18.77461439997241,100.77163198179782" },
-//         { nName: "Wat Phra That Chang Kham Worawihan", geo: "18.776243109724568,100.77198188145837" },
-//         { nName: "Wat Ming Muang", geo: "18.774720233920483,100.7691170465981" },
-//         { nName: "Wat Sri Panton", geo: "18.77583810590887,100.7657209960688" },
-//         { nName: "Wat Hua Khuang", geo: "18.777246890574332,100.77124599581259" }
-//     ]
-//     const current = "18.77519790559999,100.77440202590455" //เริ่มที่ ส.ภ. น่าน
-//     ///////////////////
-
-//     let STP = new Algo()
-//     STP.getpath(listNode, current).then((data) => {
-//         res.status(200).json(data)
-//     })
-// })
-
 app.use('/api/mst', (req, res) => {
     if (req.method == "GET") {
         res.status(400).json({ statusCode: 400, text: "This API not allow method 'GET' " })
